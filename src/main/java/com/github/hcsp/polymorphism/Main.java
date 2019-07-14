e0fc2d49baeebe3b4c6b47cf383737c78247112d
东方来客 <39690418+westee@users.noreply.github.com>
2019-07-14 10:39:18 +0800

    yourname (#9)
    yourname (#9)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,48 +1,55 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
+    abstract static class 有名字 {
         String name;
 
-        会跑(String name) {
+        有名字(String name) {
             this.name = name;
         }
 
-        String getName() {
+        public String getName() {
             return name;
         }
 
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+//        void 跑() {
+//            System.out.println(name + "跑啊跑啊跑");
+//        }
+    }
+
+    interface 会游泳 {
+        String getName();
+
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
+    interface 会跑{
+        String getName();
 
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+        default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
         }
     }
 
-    static class 猫 extends 会跑 {
+    static class 猫 extends 有名字 implements 会跑 {
         猫() {
             super("小花猫");
         }
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends 有名字 implements 会游泳 {
         鱼() {
             super("鱼");
         }
+    }
+
+    static class 乌龟 extends 有名字 implements  会跑, 会游泳{
+        乌龟(){
+            super("乌龟");
+        }
+
     }
 
     // 现在 乌龟 想要同时复用会跑和会游泳的代码
