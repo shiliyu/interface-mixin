3365497cc17ab071b616728d1c872af03c561ad6
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-07-15 17:55:44 +0800

    Update Main.java (#10)
    Update Main.java (#10)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,47 +1,42 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
+    static class Animal {
         String name;
 
-        会跑(String name) {
+        Animal(String name) {
             this.name = name;
         }
 
         String getName() {
             return name;
         }
+    }
+    interface 会跑 {
+        void 跑();
+    }
 
-        void 跑() {
+    interface 会游泳 {
+        void 游泳();
+    }
+
+    static class 猫 extends Animal implements 会跑 {
+        猫() {
+            super("小花猫");
+        }
+        @Override
+        public void 跑() {
             System.out.println(name + "跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
-
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
-        }
-    }
-
-    static class 猫 extends 会跑 {
-        猫() {
-            super("小花猫");
-        }
-    }
-
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends Animal implements 会游泳 {
         鱼() {
             super("鱼");
+        }
+        @Override
+        public void 游泳() {
+            System.out.println(name + "游啊游啊游");
         }
     }
 
@@ -49,8 +44,20 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+    static class 乌龟 extends Animal implements 会跑, 会游泳 {
+        乌龟() {
+            super("乌龟");
+        }
+        @Override
+        public void 跑() {
+            System.out.println(name + "跑啊跑啊跑");
+        }
+        
+        @Override
+        public void 游泳() {
+            System.out.println(name + "游啊游啊游");
+        }
+    }
 
     public static void main(String[] args) {
         new 乌龟().跑();
