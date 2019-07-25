30326fdba57b0a11074fe1f11c975c9634a5905a
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-07-26 00:12:58 +0800

    使用接口的默认方法实现mixin (#14)
    使用接口的默认方法实现mixin (#14)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,45 +1,37 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
+    static class Animal {
+        String Name;
 
-        会跑(String name) {
-            this.name = name;
+        public Animal(String name) {
+            this.Name = name;
         }
-
-        String getName() {
-            return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+        public String getName() {
+            return Name;
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
-
-        会游泳(String name) {
-            this.name = name;
+    interface 会跑 {
+        String getName();
+        default void 跑(){
+            System.out.println(getName() + "跑啊跑啊跑");
         }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+    }
+    interface 会游泳 {
+        String getName();
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
         }
     }
 
-    static class 猫 extends 会跑 {
+    static class 猫 extends Animal implements 会跑 {
         猫() {
             super("小花猫");
         }
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends Animal implements 会游泳 {
         鱼() {
             super("鱼");
         }
@@ -49,8 +41,11 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+     static class 乌龟 extends Animal implements 会跑,会游泳 {
+        乌龟() {
+            super("乌龟");
+        }
+    }
 
     public static void main(String[] args) {
         new 乌龟().跑();
