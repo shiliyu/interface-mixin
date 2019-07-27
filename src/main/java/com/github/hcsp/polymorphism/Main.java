9f7e7b20c56b1db37a45ca0ff1acf655a27fe972
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-27 19:07:27 +0800

    1 (#15)
    1 (#15)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,45 +1,41 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
+    interface 会跑 {
+        String getName();
 
-        会跑(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+        default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
+    interface 会游泳{
+        String getName();
 
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
         }
     }
 
-    static class 猫 extends 会跑 {
+    abstract static class Animals{
+        String name;
+
+        public Animals(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    static class 猫 extends Animals implements 会跑 {
         猫() {
             super("小花猫");
         }
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends Animals implements 会游泳 {
         鱼() {
             super("鱼");
         }
@@ -49,10 +45,14 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+    static class 乌龟 extends Animals implements 会游泳,会跑 {
 
-    public static void main(String[] args) {
+        乌龟() {
+            super("乌龟");
+        }
+    }
+
+        public static void main(String[] args) {
         new 乌龟().跑();
         new 乌龟().游泳();
     }
