b1dcee7787e0ff9a4b0e64daf5ce490f8d1f7529
Moon <justwe77@163.com>
2019-07-28 20:49:52 +0800

    modify

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,47 +1,51 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
-        String name;
+      interface 会跑 {
 
-        会跑(String name) {
-            this.name = name;
-        }
 
-        String getName() {
-            return name;
-        }
+          String getName();
 
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+          default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
+    interface 会游泳 {
+        String getName();
 
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
         }
     }
 
-    static class 猫 extends 会跑 {
+    static class Animal{
+          String name;
+
+        public Animal(String name) {
+            this.name = name;
+        }
+    }
+
+    static class 猫 extends Animal implements 会跑 {
         猫() {
             super("小花猫");
         }
+
+        @Override
+        public String getName() {
+            return "小花猫";
+        }
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends Animal implements 会游泳 {
         鱼() {
             super("鱼");
+        }
+
+        @Override
+        public String getName() {
+            return "鱼";
         }
     }
 
@@ -49,8 +53,17 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+     static class 乌龟 extends Animal implements 会游泳, 会跑 {
+        乌龟() {
+            super("乌龟");
+        }
+
+
+        @Override
+        public String getName() {
+            return "乌龟";
+        }
+    }
 
     public static void main(String[] args) {
         new 乌龟().跑();
