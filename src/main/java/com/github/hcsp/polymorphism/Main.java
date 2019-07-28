dc80ac44e4d3b9f8e159fb7fb666c8e1bbc77803
Zhang Xin <zhangxindev@gmail.com>
2019-07-28 12:53:01 +0800

    使用接口的默认方法实现mixin

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,45 +1,43 @@
 package com.github.hcsp.polymorphism;
 
+import javax.naming.Name;
+
 public class Main {
-    abstract static class 会跑 {
-        String name;
-
-        会跑(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+    interface 会跑 {
+        String getName();
+        default void 跑() {
+            System.out.println(getName() + "跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
+    interface 会游泳 {
 
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
+        String getName();
+        default void 游泳() {
+            System.out.println(getName() + "游啊游啊游");
         }
     }
 
-    static class 猫 extends 会跑 {
+    public abstract class Animal implements 会跑{
+        String name;
+
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
+    static class 猫 extends Animal implements 会跑 {
         猫() {
             super("小花猫");
         }
+
     }
 
-    static class 鱼 extends 会游泳 {
+    static class 鱼 extends Animal implements 会游泳 {
         鱼() {
             super("鱼");
         }
@@ -49,8 +47,11 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+     static class 乌龟 extends Animal implements 会跑, 会游泳 {
+        乌龟 () {
+            super("乌龟");
+        }
+     }
 
     public static void main(String[] args) {
         new 乌龟().跑();
